2ad8912dde00bd949a0d359e77800afbb8164aa5
dev_grp <groeipakket_dev@kindengezin.be>
2021-09-28 14:38:35 +0200

    Add tests for equipWeapon with Axe and Spear

--- src/test/java/be/cegeka/battle/SoldierTest.java
+++ src/test/java/be/cegeka/battle/SoldierTest.java
@@ -44,4 +44,19 @@ public class SoldierTest {
         assertThat(soldier.getWeapon()).isInstanceOf(Sword.class);
     }
 
+    @Test
+    public void givenSoldier_whenSetAxeAsWeapon_thenSoldierHasAxe() {
+        Soldier soldier = new Soldier(SOLDIER_NAME);
+        soldier.equipWeapon(new Axe());
+
+        assertThat(soldier.getWeapon()).isInstanceOf(Axe.class);
+    }
+
+    @Test
+    public void givenSoldier_whenSetSpearAsWeapon_thenSoldierHasSpear() {
+        Soldier soldier = new Soldier(SOLDIER_NAME);
+        soldier.equipWeapon(new Spear());
+
+        assertThat(soldier.getWeapon()).isInstanceOf(Spear.class);
+    }
 }
