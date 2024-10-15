a3ff7cabb1b0b6131278b6cf7814a52c981d8678
David Desopper <david.desopper@cegeka.com>
2024-10-15 16:43:03 +0200

    US3: Added strenghts to weapons

--- src/main/java/be/cegeka/battle/weapon/Weapon.java
+++ src/main/java/be/cegeka/battle/weapon/Weapon.java
@@ -1,8 +1,18 @@
 package be.cegeka.battle.weapon;
 
 public enum Weapon {
-    BARE_FIST,
-    AXE,
-    SWORD,
-    SPEAR
+    BARE_FIST(1),
+    AXE(3),
+    SWORD(2),
+    SPEAR(2);
+
+    private final int weaponStrength;
+
+    Weapon(int weaponStrength) {
+        this.weaponStrength = weaponStrength;
+    }
+
+    public int getWeaponStrength() {
+        return weaponStrength;
+    }
 }
---
+++ src/test/java/be/cegeka/battle/weapon/WeaponTest.java
@@ -0,0 +1,26 @@
+package be.cegeka.battle.weapon;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WeaponTest {
+
+    @ParameterizedTest
+    @MethodSource("weaponAndStrenghts")
+    void getWeaponStrength(Weapon weapon, int expectedStrength) {
+        assertThat(weapon.getWeaponStrength()).isEqualTo(expectedStrength);
+    }
+
+    private static Stream<Arguments> weaponAndStrenghts() {
+        return Stream.of(
+                Arguments.of(Weapon.BARE_FIST, 1),
+                Arguments.of(Weapon.AXE, 3),
+                Arguments.of(Weapon.SWORD, 2),
+                Arguments.of(Weapon.SPEAR, 2));
+    }
+}
