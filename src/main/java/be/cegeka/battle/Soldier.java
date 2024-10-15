c355a515a79a33cec72c628bc6fd2e6eacdfb30e
David Desopper <david.desopper@cegeka.com>
2024-10-15 16:32:51 +0200

    US1: Refactor validity of name

--- src/main/java/be/cegeka/battle/Soldier.java
+++ src/main/java/be/cegeka/battle/Soldier.java
@@ -1,21 +1,23 @@
 package be.cegeka.battle;
 
 
-import com.google.common.base.Strings;
-
 public class Soldier {
 
     private final String name;
 
     public Soldier(String name) {
-        if (Strings.isNullOrEmpty(name)) {
-            throw new IllegalArgumentException();
-        }
+        checkIfNameIsValid(name);
 
         this.name = name;
     }
 
     String getName() {
         return this.name;
+    }
+
+    private static void checkIfNameIsValid(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("A soldier must have a name");
+        }
     }
 }
